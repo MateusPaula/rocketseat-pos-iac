6b23492a0aa30d4e7057cb07b10a0a01edf651cb
Mateus Paula <mateuspaula@Laptop-de-Mateus.local>
2025-06-22 20:34:53 -0300

    feat: first commit with pulumi

---
+++ index.ts
@@ -0,0 +1,23 @@
+import * as aws from "@pulumi/aws";
+
+const bucket = new aws.s3.BucketV2("primeiro-bucket-s3", {
+    bucket: 'primeiro-bucket',
+    tags: {
+        IAC: "true",
+    }
+});
+
+const ecr = new aws.ecr.Repository("primeir-ecr", {
+    name: 'primeiro-ecr',
+    imageTagMutability: 'IMMUTABLE',
+    tags: {
+        IAC: "true"
+    }
+})
+
+export const bucketName = bucket.id;
+export const bucketInfo = bucket.bucket;
+export const bucketArn = bucket.arn;
+
+export const ecrName = ecr.name;
+export const ecrRepositoryUrl = ecr.repositoryUrl;
