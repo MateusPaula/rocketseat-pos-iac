d05d974452aadb19dd7a2571694b7ccb2b990bbe
Mateus Paula <mateuspaula@Laptop-de-Mateus.local>
2025-06-22 21:50:25 -0300

    fix: changing bucket name

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
 import * as aws from "@pulumi/aws";
 
 const firstBucket = new aws.s3.BucketV2("first-bucket", {
-    bucket: 'primeiro-bucket',
+    bucket: 'first-bucket',
     tags: {
         IAC: "true",
     }
 });
 
 const secondBucket = new aws.s3.BucketV2("second-bucket", {
-    bucket: 'primeiro-bucket',
+    bucket: 'second-bucket',
     tags: {
         IAC: "true",
     }
 });
 
 const ecr = new aws.ecr.Repository("primeir-ecr", {
-    name: 'primeiro-ecr',
+    name: 'primeiro-ecr-pos',
     imageTagMutability: 'IMMUTABLE',
     tags: {
         IAC: "true"
